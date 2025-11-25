d4825a0a4981b74f09a75b074489d27016e919ac
razanekaraja <rwk353@student.bau.edu.lb>
2025-11-26 01:00:15 +0200

    FIX: Remove unnecessary code/variable from HomeController.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController {
         $plantModel = new Plant();
         $plants = $plantModel->getAllPlants(); 
 
-        //echo "<h1>Database Connection: SUCCESS!</h1>"; chlt hal code ba3d ma tl3li Database Connection: SUCCESS! mn app/controllers/HomeController.php
+        //echo "<h1>Database Connection: SUCCESS!</h1>"; 
 
         require_once ROOT_PATH . 'app/views/home.php'; 
     }
